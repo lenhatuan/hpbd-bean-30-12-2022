ed9850965cd43ea24f9408f9721b025e47329d6f
tuan.le11 <tuan.le@bizzi.vn>
2022-12-27 20:49:20 +0700

    Commit

---
+++ config.js
@@ -0,0 +1,22 @@
+var config = {
+    texts: [
+        "You are all I need",
+        "My sugar honey",
+        "You're incredible",
+        "On your birthday today",
+        "I just want you to know",
+        "How lucky I am to have you in my life",
+        "Happy birthday to you!",
+        "MY LOVE",
+    ],
+    imgs: {
+        "My sugar honey": "./imgs/bean1.jpg",
+        "You're incredible": "./imgs/bean2.jpg",
+        "How lucky I am to have you in my life": "./imgs/bean5.jpg",
+        "Happy birthday to you!": "./imgs/bean4.jpg",
+        "MY LOVE": "./imgs/bean6.jpg"
+    },
+    desc: {
+        story: "A MESSAGE FOR YOU",
+    }
+};
